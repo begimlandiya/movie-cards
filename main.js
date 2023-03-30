a5cc570f150647a3c17bde55dd24a642311f9d9f
begimlandiya <erkinovabegimai05@gmail.com>
2023-03-30 17:04:42 +0600

    first commit

---
+++ main.js
@@ -0,0 +1,32 @@
+// todo Good Luck To Me
+
+let cards = document.querySelector(".cards")
+
+let linkInf = fetch("https://www.omdbapi.com/?apikey=b6003d8a&s=All")
+
+
+linkInf.then((response) => {
+    return response.json();
+  })
+    .then((info) => {
+      console.log(info.Search);
+      cards.innerHTML = ""
+      info.Search.forEach((elem) => {
+          cards.innerHTML += `
+          <div class="card">
+        <img
+          class="img"
+          src="${elem.Poster}"
+          alt=""
+        />
+        <div class="text">
+          <span class="desc">${elem.Title}</span>   <span class="year">${elem.Year}</span>
+        </div>
+      </div>
+    
+      
+          `;  
+      });
+    })
+    
+  
